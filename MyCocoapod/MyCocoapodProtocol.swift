d26a55f79885001d99845b5558375579227a1bc5
octopused <octopused@gmail.com>
2020-12-24 11:55:51 +0500

    Initial commit

---
+++ MyCocoapod/MyCocoapodProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  MyCocoapodProtocol.swift
+//  MyCocoapod
+//
+//  Created by r.mingazov on 23.12.2020.
+//
+
+import Foundation
+
+public protocol MyCocoapodProtocol {
+    func some() {
+    
+    }
+}
---
+++ MyCocoapod/MyCococapodClass.swift
@@ -0,0 +1,14 @@
+//
+//  MyCococapodClass.swift
+//  MyCocoapod
+//
+//  Created by r.mingazov on 23.12.2020.
+//
+
+import Foundation
+
+open class MyCocoapodClass: MyCocoapodProtocol {
+    public func some() {
+        
+    }
+}
